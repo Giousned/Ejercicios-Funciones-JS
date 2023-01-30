93d4ffaa9ca796d16bdb6d2ec94af8bb33cf5e96
Giousned <guille_ranza@hotmail.com>
2023-01-30 13:53:10 +0000

    [ADD] correcciones y mejoras con nuevos conocimientos

--- exercises/08-Function-parameters/app.js
+++ exercises/08-Function-parameters/app.js
@@ -9,9 +9,21 @@ let genero;
 */
 
 function renderPerson(nombre,año,color,edad,genero) {
-  return nombre+""+" is a "+""+edad+""+" years old "+""+genero+""+" born in "+""+año+""+" with "+""+color+""+" eyes";
+  return `${nombre} is a ${edad} years old ${genero} born in ${año} with ${color} eyes`;
 }
 
 
   ///*** Do not edit below this line ****/
   console.log(renderPerson('Bob', '05/22/1983', 'green', 23, 'male'));
+
+/* OTRA SOLUCION
+function renderPerson(nombre,año,color,edad,genero) {
+  return nombre+""+" is a "+""+edad+""+" years old "+""+genero+""+" born in "+""+año+""+" with "+""+color+""+" eyes";
+}
+*/
+
+/* USANDO TEMPLATE con el acento grave para abrir y cerrar y con los $ y llaves para las variables
+function renderPerson(nombre,año,color,edad,genero) {
+  return `${nombre} is a ${edad} years old ${genero} born in ${año} with ${color} eyes`;
+}
+*/
--- exercises/10-Remove-vowels/app.js
+++ exercises/10-Remove-vowels/app.js
@@ -1,4 +1,26 @@
 const rapid = (myString) => {
+    
+    let consonants = [];
+
+    let regex = /^[aeiou]/i;
+    
+    for(let letter of myString){
+        
+        if(regex.test(letter) === false)
+            consonants.push(letter)
+    }
+      
+    return consonants.join('').toUpperCase();
+};
+
+
+// Work above this line; do not change code below
+let str = "John";
+console.log(rapid(str));
+
+
+/* SOLUCION DADA
+const rapid = (myString) => {
     let consonants = [];
     for(let letter of myString){
         if(['a','e','i','o','u'].includes(letter) == false)
@@ -6,8 +28,4 @@ const rapid = (myString) => {
     }
       return consonants.join('').toUpperCase();
 };
-
-
-// Work above this line; do not change code below
-let str = "John";
-console.log(rapid(str));
+*/
